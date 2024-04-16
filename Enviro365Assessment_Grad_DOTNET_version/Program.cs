79da8b4a86cf0c1f7fd0f0af6aab38933f99eba7
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-16 18:34:35 +0200

    Configure swagger

--- Enviro365Assessment_Grad_DOTNET_version/Program.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Program.cs
@@ -1,15 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(opt =>
+{
+    opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Enviro365 Environment API V1");
+    opt.RoutePrefix = string.Empty;
+    opt.DocumentTitle = "Enviro365 Environment API";
+});
 
-app.UseHttpsRedirection();
+app.MapControllers();
 
 app.MapGet("/test", () => "Hello World!");
+
 app.Run();
