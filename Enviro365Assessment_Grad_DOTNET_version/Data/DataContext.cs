55989bc305d1212369b817f45c67cce61ee8a86e
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 00:13:35 +0200

    Configure db to dbcontext

---
+++ Enviro365Assessment_Grad_DOTNET_version/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Enviro365Assessment_Grad_DOTNET_version.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("FileName=environmentdb.sqlite");
+    }
+
+}
