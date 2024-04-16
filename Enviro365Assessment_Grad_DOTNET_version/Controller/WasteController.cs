fb5a966a7a45ca73959a7949fe3ad89da18c02a3
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-16 19:43:03 +0200

    Add controller class

---
+++ Enviro365Assessment_Grad_DOTNET_version/Controller/WasteController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enviro365Assessment_Grad_DOTNET_version.Controller;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class WasteController : ControllerBase
+{
+}
