8d9f1831d1c044b069d687554254ae1618596550
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 01:52:47 +0200

    Implement save waste data

--- Enviro365Assessment_Grad_DOTNET_version/Controller/WasteController.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Controller/WasteController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Enviro365Assessment_Grad_DOTNET_version.Data;
+using Enviro365Assessment_Grad_DOTNET_version.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Enviro365Assessment_Grad_DOTNET_version.Controller;
 
@@ -6,6 +9,24 @@ namespace Enviro365Assessment_Grad_DOTNET_version.Controller;
 [Route("/api/[controller]")]
 public class WasteController : ControllerBase
 {
-    [HttpGet]
-    public ActionResult<string> Test() => "Test";
+    private DataContext _dataContext;
+
+    public WasteController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+    [HttpPost]
+    public ActionResult<ResponseObject> SaveWaste(Waste waste)
+    {
+        try
+        {
+            _dataContext.Add(waste);
+            int rowsAffected = _dataContext.SaveChanges();
+            return (rowsAffected > 0) ? new ResponseObject("Waste saved successfully.", 200) : new ResponseObject("Waste not saved.", 400);
+        }
+        catch (Exception e)
+        {
+            return new ResponseObject("An error occurred while saving the waste.", ((int)HttpStatusCode.InternalServerError));
+        }
+    }
 }
