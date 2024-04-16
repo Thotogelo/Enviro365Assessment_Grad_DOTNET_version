26fb3fdbbdb3869f7372456d2dee522cb88e421b
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 01:14:23 +0200

    Configure npsql

--- Enviro365Assessment_Grad_DOTNET_version/Data/DataContext.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Data/DataContext.cs
@@ -6,19 +6,16 @@ namespace Enviro365Assessment_Grad_DOTNET_version.Data;
 
 public class DataContext : DbContext
 {
+    private readonly IConfiguration _configuration;
     public DbSet<Waste> Wastes { get; set; }
 
     public DataContext(DbContextOptions options) : base(options)
     {
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite("FileName=environmentdb.sqlite");
-    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
         new WasteModelConfig().Configure(modelBuilder.Entity<Waste>());
-    }
 }
