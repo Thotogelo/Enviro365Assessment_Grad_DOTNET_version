6f804473a437ae36821bef80424101af766171d5
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-16 19:39:03 +0200

    Add model

---
+++ Enviro365Assessment_Grad_DOTNET_version/Model/Waste.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Enviro365Assessment_Grad_DOTNET_version.Model;
+
+[Table("Waste")]
+public class Waste
+{
+    [Key]
+    [Required]
+    [Column(name: "wasteid")]
+    public long Id { get; set; }
+
+    [Required]
+    [Column(name: "waste_category", TypeName = "varchar(10)")]
+    public string Category { get; set; }
+
+    [Required]
+    [Column(name: "disposal_guideline", TypeName = "varchar(50)")]
+    public string Disposalguideline { get; set; }
+
+    [Required]
+    [Column(name: "recycling_tips", TypeName = "varchar(100)")]
+    public string Recyclingtips { get; set; }
+}
