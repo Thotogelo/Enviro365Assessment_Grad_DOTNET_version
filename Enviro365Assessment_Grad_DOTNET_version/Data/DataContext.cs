8a270dbcc7686cc2de8c11ca79bdf35711f5c00a
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 00:42:03 +0200

    Config dbcontext

--- Enviro365Assessment_Grad_DOTNET_version/Data/DataContext.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Data/DataContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Enviro365Assessment_Grad_DOTNET_version.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Enviro365Assessment_Grad_DOTNET_version.Data;
 
 public class DataContext : DbContext
 {
+    public DbSet<Waste> Wastes { get; set; }
+
     public DataContext(DbContextOptions options) : base(options)
     {
     }
@@ -13,4 +17,8 @@ public class DataContext : DbContext
         optionsBuilder.UseSqlite("FileName=environmentdb.sqlite");
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        new WasteModelConfig().Configure(modelBuilder.Entity<Waste>());
+    }
 }
