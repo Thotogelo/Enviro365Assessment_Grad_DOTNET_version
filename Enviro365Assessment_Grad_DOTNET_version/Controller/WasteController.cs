7964b03bf85ed5da15f958bec0d644b96551bc18
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 08:54:18 +0200

    Implement savewaste. ProblemDetails

--- Enviro365Assessment_Grad_DOTNET_version/Controller/WasteController.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Controller/WasteController.cs
@@ -2,7 +2,6 @@
 using Enviro365Assessment_Grad_DOTNET_version.Data;
 using Enviro365Assessment_Grad_DOTNET_version.Model;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace Enviro365Assessment_Grad_DOTNET_version.Controller;
 
@@ -19,23 +18,23 @@ public class WasteController : ControllerBase
     [HttpPost]
     [Consumes("application/json")]
     [Produces("application/json")]
-    public ActionResult<ProblemDetails> SaveWaste(Waste waste)
+    public IActionResult SaveWaste(Waste waste)
     {
         try
         {
             _dataContext.Add(waste);
             int rowsAffected = _dataContext.SaveChanges();
-            // return (rowsAffected > 0) ? new ResponseObject("Waste saved successfully.", 200) : new ResponseObject("Waste not saved.", 400);
-            return (rowsAffected > 0) ? new ProblemDetails { Title = "Waste saved successfully.", Status = 200 } : new ProblemDetails { Title = "Waste not saved.", Status = 400 };
+            return (rowsAffected > 0)
+                ? Ok(new ProblemDetails { Title = "Waste saved successfully.", Status = (int)HttpStatusCode.Created })
+                : BadRequest(new ProblemDetails { Title = "Waste not saved.", Status = (int)HttpStatusCode.InternalServerError });
         }
         catch (Exception e)
         {
-            // return new ResponseObject(e.Message, (int)HttpStatusCode.InternalServerError);
-            return new ProblemDetails
+            return BadRequest(new ProblemDetails
             {
                 Title = e.Message,
                 Status = (int)HttpStatusCode.InternalServerError
-            };
+            });
         }
     }
 }
