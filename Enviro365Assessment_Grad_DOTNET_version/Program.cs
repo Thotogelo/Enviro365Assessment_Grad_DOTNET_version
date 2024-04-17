ae128371eabed0f7c6f416f4bee60e8234f8f4c0
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 08:53:37 +0200

    Register problemdetails for di, service

--- Enviro365Assessment_Grad_DOTNET_version/Program.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>();
+builder.Services.AddProblemDetails();
 
 //Add DI
 builder.Services.AddTransient<DataContext>();
