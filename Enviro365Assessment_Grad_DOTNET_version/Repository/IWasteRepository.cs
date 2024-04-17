200279df7c202aea129d55f5334fc24a36c8cd36
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 18:00:49 +0200

    Add Interface

--- Enviro365Assessment_Grad_DOTNET_version/Repository/IWasteRepository.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Repository/IWasteRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace Enviro365Assessment_Grad_DOTNET_version.Repository;
+﻿using Enviro365Assessment_Grad_DOTNET_version.Model;
+
+namespace Enviro365Assessment_Grad_DOTNET_version.Repository;
 
 public interface IWasteRepository
 {
-    
+    Waste GetWasteById(long Id);
+
+    List<Waste> GetWasteListByCategory(string category);
+
+    List<Waste> GetAllWaste();
+
+    int SaveWaste(Waste waste);
+
+    int UpdateWaste(Waste waste);
+
+    int DeleteWasteById(long id);
+
+    int DeleteWasteListByCategory(string category);
 }
