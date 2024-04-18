e2428428e929e4b54eb3f704eca4d87f82e83a7d
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-18 23:24:14 +0200

    Example global exception handler Within comments

--- Enviro365Assessment_Grad_DOTNET_version/Program.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Program.cs
@@ -1,4 +1,6 @@
+using Enviro365Assessment_Grad_DOTNET_version;
 using Enviro365Assessment_Grad_DOTNET_version.Data;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +11,10 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>();
 builder.Services.AddProblemDetails();
 
+builder.Logging.AddConsole();
+
 //Add DI
 builder.Services.AddTransient<DataContext>();
-
 var app = builder.Build();
 
 app.UseSwagger();
@@ -20,6 +23,35 @@ app.UseSwaggerUI(opt =>
     opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Enviro365 Environment API V1");
     opt.RoutePrefix = string.Empty;
     opt.DocumentTitle = "Enviro365 Environment API";
+});
+
+// Use the custom error handler middleware
+app.UseMiddleware<ErrorHandlerMiddleware>();
+// app.UseExceptionHandler(appError =>
+// {
+//     appError.Run(async ctx =>
+//     {
+//         ctx.Response.StatusCode = 500;
+//         ctx.Response.ContentType = "application/json";
+
+//         var ctxFeature = ctx.Features.Get<IExceptionHandlerFeature>();
+
+//         if (ctxFeature is not null)
+//         {
+//             Console.WriteLine("Error: " + ctxFeature.Error);
+//             await ctx.Response.WriteAsJsonAsync(new
+//             {
+//                 ctx.Response.StatusCode,
+//                 Message = "Internal Server Error"
+//             });
+
+//         }
+//     });
+// });
+
+app.MapGet("/demo", () =>
+{
+    throw new WasteError();
 });
 
 app.MapControllers();
