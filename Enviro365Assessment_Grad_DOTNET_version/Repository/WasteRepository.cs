88e7891f97a7e9aa7e864f34e7b8d5aecafac1a7
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-19 16:48:31 +0200

    Import implementations into wasterepository

--- Enviro365Assessment_Grad_DOTNET_version/Repository/WasteRepository.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Repository/WasteRepository.cs
@@ -1,6 +1,41 @@
-﻿namespace Enviro365Assessment_Grad_DOTNET_version.Repository;
+﻿using Enviro365Assessment_Grad_DOTNET_version.Model;
 
-public class WasteRepository
+namespace Enviro365Assessment_Grad_DOTNET_version.Repository;
+
+public class WasteRepository : IWasteRepository
 {
-    
+    public Waste GetWasteById(long Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Waste> GetWasteListByCategory(string category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Waste> GetAllWaste()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int SaveWaste(Waste waste)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int UpdateWaste(Waste waste)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int DeleteWasteById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int DeleteWasteListByCategory(string category)
+    {
+        throw new NotImplementedException();
+    }
 }
