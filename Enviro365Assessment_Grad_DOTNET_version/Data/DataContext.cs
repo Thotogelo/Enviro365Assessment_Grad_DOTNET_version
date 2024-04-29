454e5aab137cfde8a441fe8260a6695f0c4bdcde
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-29 10:42:00 +0200

    Improve imports

--- Enviro365Assessment_Grad_DOTNET_version/Data/DataContext.cs
+++ Enviro365Assessment_Grad_DOTNET_version/Data/DataContext.cs
@@ -1,6 +1,5 @@
 ﻿using Enviro365Assessment_Grad_DOTNET_version.Model;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Enviro365Assessment_Grad_DOTNET_version.Data;
 
